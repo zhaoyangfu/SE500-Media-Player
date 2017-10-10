3713c65efaa5065dcfb48861e4f1cee58589723b
hudagh <32314895+hudagh@users.noreply.github.com>
2017-10-10 14:13:19 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -12,7 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,800,400);
+			//write in the border or the stage 
+			primaryStage.setTitle("Media Player");
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();

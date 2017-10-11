74f8ce51af3e03a07a1fcb806a0277f1b72e98eb
hudagh <32314895+hudagh@users.noreply.github.com>
2017-10-11 17:04:42 -0600

    updated application

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,65 +1,29 @@
 package application;
-
-import java.io.File;
-import java.net.MalformedURLException;
-
+	
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
 
 public class Main extends Application {
-	Player player;
-	FileChooser fileChooser = new FileChooser();
-
-	public void start(final Stage primaryStage) {
-		
-		MenuItem open = new MenuItem("open");
-		Menu file = new Menu("File");
-		MenuBar menu = new MenuBar();
-		
-		file.getItems().add(open);
-		menu.getMenus().add(file);
-		
-		open.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent e) {
-				player.player.pause();
-				player.bar.playButton.setText(">");
-				File filec = fileChooser.showOpenDialog(primaryStage);
-				if (filec != null) {
-					try {
-						player = new Player(filec.toURI().toURL().toExternalForm());
-						player.setTop(menu);
-						Scene scene = new Scene(player, 640, 480);
-						primaryStage.setScene(scene);
-						player.player.setOnReady(new Runnable() {
-							@Override
-							public void run() {
-								primaryStage.setWidth(player.player.getMedia().getWidth() + 15);
-								primaryStage.setHeight(player.player.getMedia().getHeight() + 120);
-								primaryStage.setMinWidth(640);
-								primaryStage.setMinHeight(480);
-							}
-						});
-					} catch (MalformedURLException e1) {
-						e1.printStackTrace();
-					}
-					
-				}
-			}
-		});
-		player = new Player("file:///C:/Users/Jackie/Downloads/test/toystory.mp4");
-		player.setTop(menu);
-		Scene scene = new Scene(player, 640, 380);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,800,400);
+			//write in the border or the stage 
+			primaryStage.setTitle("Media Player");
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
